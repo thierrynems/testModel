fb7f1330ddd1d1972c84964eb71c5683acbf1ee2
KONGNE N. Thierry <thierrynemsk@gmail.com>
2022-05-01 21:48:10 -0400

    Update MLModelFull-RandomForest.py
    remove min_impurity_split=None,

--- MLModelFull-RandomForest.py
+++ MLModelFull-RandomForest.py
@@ -207,10 +207,10 @@ for index in range(row) :
 
 	rf_model = RandomForestClassifier(n_estimators=200)
 	rf_model.fit(X_train_scaled, y_train)
-
+	#min_impurity_split=None,
 	RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
 	            max_depth=None, max_features='auto', max_leaf_nodes=None,
-	            min_impurity_decrease=0.0, min_impurity_split=None,
+	            min_impurity_decrease=0.0, 
 	            min_samples_leaf=1, min_samples_split=2,
 	            min_weight_fraction_leaf=0.0, n_estimators=200, n_jobs=None,
 	            oob_score=False, random_state=None, verbose=0,
